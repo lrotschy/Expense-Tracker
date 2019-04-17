7b2dbf62b2e7388d04fd0e4547da5dbc9c32f4ff
Liela Rotschy <lielarotschy@gmail.com>
2019-04-17 09:34:27 -0400

    initial project files through assignment 11

---
+++ schema.sql
@@ -0,0 +1,36 @@
+-- CREATE DATABASE expenses;
+
+CREATE TABLE expenses (
+  id serial PRIMARY KEY,
+  amount decimal(6, 2) NOT NULL,
+  memo text NOT NULL,
+  created_on date NOT NULL
+);
+
+ALTER TABLE expenses ADD CONSTRAINT non_zero_amount CHECK (amount > 0);
+
+
+INSERT INTO expenses (amount, memo, created_on) VALUES
+(9999.99, 'biggest', '2019-04-16');
+
+INSERT INTO expenses (amount, memo, created_on) VALUES
+(10000.00, 'too big', '2019-04-16');
+
+INSERT INTO expenses (amount, memo, created_on) VALUES
+(0000.00, 'smallest', '2019-04-16');
+
+INSERT INTO expenses (amount, memo, created_on) VALUES
+(-9999.99, 'actual smallest', '2019-04-16');
+
+DELETE FROM expenses WHERE amount <= 0;
+
+ALTER TABLE expenses ADD CONSTRAINT non_zero_amount CHECK (amount > 0);
+
+INSERT INTO expenses (amount, memo, created_on) VALUES
+(-00.01, 'too small', '2019-04-16');
+
+INSERT INTO expenses (amount, memo, created_on) VALUES
+(-00.01, 'too small', '2019-04-16'),
+(14.56, 'pencils', NOW()),
+(3.29, 'coffee', NOW()),
+(49.99 'solar panel', NOW());
